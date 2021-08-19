8d6da31e1658eef43151671e171fee9fce09e923
nikoladanilovic <nikola.danilovic28@gmail.com>
2021-08-19 17:54:21 +0200

    Deletion of location is synced with deletion of its objects

--- app/src/main/java/hr/ferit/nikoladanilovic/objectfinder/adapter/LocationsListHolder.kt
+++ app/src/main/java/hr/ferit/nikoladanilovic/objectfinder/adapter/LocationsListHolder.kt
@@ -2,6 +2,7 @@ package hr.ferit.nikoladanilovic.objectfinder.adapter
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.util.Log
 import android.view.View
 import androidx.core.content.ContextCompat.startActivity
@@ -15,6 +16,8 @@ import hr.ferit.nikoladanilovic.objectfinder.NewLocationActivity
 import hr.ferit.nikoladanilovic.objectfinder.ObjectListOnLocationActivity
 import hr.ferit.nikoladanilovic.objectfinder.databinding.LocationsItemBinding
 import hr.ferit.nikoladanilovic.objectfinder.model.Location
+import hr.ferit.nikoladanilovic.objectfinder.model.ObjectOfInterest
+import java.io.File
 
 class LocationsListHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
     //FirebaseAuth
@@ -63,15 +66,29 @@ class LocationsListHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
                 .addOnFailureListener { exception ->
                     Log.d(TAG, "Error getting documents: ", exception)
                 }
+
+            //TREBAT CE SE OVJDE IMPLEMENTIRATI I BRISANJE SVIH OBJEKATA KOJE OPISUJE PRIPADNA LOKACIJA KOJA SE BRISE!!
+
+            usersRef.whereEqualTo("email", email).get()
+                .addOnSuccessListener { result ->
+                    for (document in result) {
+                        document.reference.collection("objects").whereEqualTo("locationId", deletedLocation.getDocumentId()).get()
+                            .addOnSuccessListener { result2 ->
+                                for (document2 in result2) {
+                                    val objectFromFirebase = document2.toObject(ObjectOfInterest::class.java)
+                                    val imageUri = Uri.parse(objectFromFirebase.getUriOfImage())
+                                    val fdelete = File(imageUri.path!!)
+                                    fdelete.delete()
+
+                                    document2.reference.delete()
+                                }
+                            }
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.d(TAG, "Error getting documents: ", exception)
+                }
+
         }
-
-        //TREBAT CE SE OVJDE IMPLEMENTIRATI I BRISANJE SVIH OBJEKATA KOJE OPISUJE PRIPADNA LOKACIJA KOJA SE BRISE!!
-
+        }
     }
-
-
-
-
-
-
-}
